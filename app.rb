73fd06a2b7a336d3cfb7adc622629e4076b54536
Lauren McCoy <lmccoy8911@gmail.com>
2019-08-16 22:30:47 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
+  get '/newteam' do 
+    erb :newteam 
+  end 
+
+  post '/team' do 
+    @team_name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @pf = params[:pf]
+    @sg = params[:sg]
+    @c = params[:c]
+    #binding.pry 
+    erb :team 
+  end 
 
 end
