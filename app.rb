d5c9eb39584796c36df641720c722c56211551de
Rohan Phillips <rohan@solutions-insurance.com>
2019-09-05 16:01:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,14 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @teamdata = params
+    puts @teamdata
+    erb :team
+  end
 
 end
