ee69e51c693502c72e54df3da649ccf5303d2f3d
Ronald Stephens <rstephens@bluemorpho.cc>
2019-11-15 02:11:33 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,13 +7,14 @@ class App < Sinatra::Base
   end
 
   post '/newteam' do
-    @name = params["name"]
-    @coach = params["coach"]
-    @pg = params["pg"]
-    @sg = params["sg"]
-    @sf = params["sf"]
-    @pf = params["pf"]
-    @c = params["c"]
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+
     erb :team
   end
 
