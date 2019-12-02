850e14aed28a48ce0a24bc409e99fecfad4d3f95
Christopher Dent <christopherdent01@gmail.com>
2019-12-01 20:56:26 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,24 @@
+require_relative 'config/environment'
 require 'sinatra/base'
 
 class App < Sinatra::Base
 
+
+  get '/newteam' do
+    puts params
+    erb :newteam
+  end
+
+  post '/newteam' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+
+    erb :team
+  end
 
 end
