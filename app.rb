411b12f96691cdb291f497eb60f225b3df46187f
Chris Cheong <chrisxcheong@gmail.com>
2020-01-26 07:16:52 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,20 +1,20 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+
   get '/newteam' do
     erb :newteam
   end
 
   post '/team' do
-    @name =params[:name]
+    @name = params[:name]
     @coach = params[:coach]
     @pg = params[:pg]
     @sg = params[:sg]
-    @sf = params[:sf]
     @pf = params[:pf]
+    @sf = params[:sf]
     @c = params[:c]
-
-    erb: team
-
+    erb :team
   end
+
 end
