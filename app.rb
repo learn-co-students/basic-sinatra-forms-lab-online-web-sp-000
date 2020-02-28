64ba2779efc6f9f6e59273d2748cd5c46cdf6acc
Michael Carroll <carrollm2@gmail.com>
2020-02-28 03:01:01 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @name = params["name"]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @small_forward = params["small_forward"]
+    @power_forward = params["power_forward"]
+    @center = params["c"]
+    erb :team
+  end
+
 
 end
