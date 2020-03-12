18fd948bb4103b77c18b8b26fc95c2bcec1a98d6
CoderJay06 <jaycru06@yahoo.com>
2020-03-11 20:55:21 -0400

    Done.

--- app.rb
+++ app.rb
@@ -6,4 +6,16 @@ class App < Sinatra::Base
       erb :newteam 
    end 
 
+   post '/team' do 
+      @name = params[:name]
+      @coach = params[:coach]
+      @pg = params[:pg]
+      @sg = params[:sg]
+      @sf = params[:sf]
+      @pf = params[:pf]
+      @c = params[:c]
+      
+      erb :team
+   end 
+
 end
