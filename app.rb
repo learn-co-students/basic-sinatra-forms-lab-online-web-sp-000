fa6b35812ced32d16c22c0e9d09916e6480af18d
Stela Capsa <stela.capsa@gmail.com>
2020-03-29 20:11:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,30 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+ 
+  get '/' do 
+   erb :newteam
+ end 
+ 
+ get '/newteam' do 
+   erb :newteam
+ end 
+ 
+ get '/team' do 
+   erb :team 
+ end 
+ 
+ post '/team' do 
+   @name = params[:name]
+   @coach = params[:coach]
+   @pg = params[:pg]
+   @sg = params[:sg]
+   @sf = params[:sf]
+   @pf = params[:pf]
+   @center = params[:c]
+   erb :team 
+ end 
+ 
 
 
 end
