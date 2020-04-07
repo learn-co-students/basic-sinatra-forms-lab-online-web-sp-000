d2bc59dcad0a9d5ab23a0659c0f5c2ffb6f29a09
Chad Lengefeld <chadlengefeld@gmail.com>
2020-04-07 16:20:52 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
+require 'pry'
 require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+end
+
+  post '/team' do
+    # @team = team.all
+     erb :newteam
+  end
 
 end
