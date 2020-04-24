f23f518fd4d5e2963ca0f32cfdd4b234a39e9a55
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-24 22:24:05 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,24 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/newteam' do
+    erb :newteam
+  end
+  post '/newteam' do
+    erb :team
+  end
+  post '/team' do
+      @name = params[:name]
+      @coach = params[:coach]
+      @pg = params[:pg]
+      @pf = params[:pf]
+      @sg = params[:sg]
+      @sf = params[:sf]
+      @c = params[:c]
+
+  erb :team
+
+  end
 
 
 end
