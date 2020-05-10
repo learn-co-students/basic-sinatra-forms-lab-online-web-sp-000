e77e7c2b5547dc77a26eb68eda02e2be0aa06ffa
Mark A Guza <guzamaguza@gmail.com>
2020-05-10 00:54:00 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,13 +7,13 @@ class App < Sinatra::Base
     end
 
     post '/newteam' do
-      @name = params[:teamname]
+      @name = params[:name]
       @coach = params[:coach]
-      @pg = params[:pointguard]
-      @sg = params[:shootingguard]
-      @sf = params[:smallforward]
-      @pf = params[:powerforward]
-      @center = params[:center]
+      @pg = params[:pg]
+      @sg = params[:sg]
+      @sf = params[:sf]
+      @pf = params[:pf]
+      @c = params[:c]
 
       erb :team
     end
