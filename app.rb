5e9a706504fe150d98f18c2fc2e5bab8554327bf
jake0988 <jake098877@gmail.com>
2020-07-08 21:39:22 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
+    get '/newteam' do
+        erb :newteam
+    end
+
+    # post '/newteam' do
+    #     @team = params
+        
+    #     erb :team
+    # end
+
+    post '/team' do
+         @team = params
+        # binding.pry
+        erb :team
+    end
 
 end
