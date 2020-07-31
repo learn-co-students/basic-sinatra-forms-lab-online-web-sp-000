72ca9036d17f90ba6e6329c3750afc8b4d7d0b34
yarvinh <yarvinhhernandez@gmail.com>
2020-07-31 17:01:40 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,23 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+ 
 
 
+    get '/newteam' do  
+        erb :newteam
+    end
+
+    post '/team' do 
+        @team_name = params[:name]
+ 
+        @coach = params[:coach]
+        @cl = params[:cl]
+        @pg = params[:pg]
+        @sg = params[:sg]
+        @sf = params[:sf]
+        @pf = params[:pf]
+        @c = params[:c]
+        erb :team
+    end
 end
