47f955b8e7775e5094b5fe5170fa61e52e2e3fa5
mdacht777 <mdacht777@gmail.com>
2020-08-23 12:51:01 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+    get '/newteam' do
+    erb :newteam
+    end
 
+    post '/team' do
+        @params=params
+        erb :team
+    end
 
+    get '/friends' do
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+    
+    erb :friends
+    end
 end
