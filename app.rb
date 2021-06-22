154744b045ca663435b996bc3abf0a6acc3c0b1a
VandelayUtd <drewhadley.guitar@gmail.com>
2021-06-22 11:57:11 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,19 +1,21 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
     get '/newteam' do 
+     
         erb :newteam
     end
 
     post '/team' do
-        # binding.pry
+        
         @name = params[:name]
-        @coach = params[:coach]
+        @coach = params[:coach] 
         @pg = params[:pg]
         @sg = params[:sg]
         @sf = params[:sf]
         @pf = params[:pf]
-        @c = parms[:c]
+        @c = params[:c]
         erb :team
     end
 
